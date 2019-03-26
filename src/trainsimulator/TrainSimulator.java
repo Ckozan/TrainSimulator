6e857a953cd5c0aa538cb1d219cf6c612669cb3f
KSmith-16 <35820290+KSmith-16@users.noreply.github.com>
2019-03-25 23:18:54 -0400

    Update TrainSimulator.java
    Updated the main of the simulator

--- src/trainsimulator/TrainSimulator.java
+++ src/trainsimulator/TrainSimulator.java
@@ -8,11 +8,28 @@ public class TrainSimulator {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        boolean shouldContinue = false; 
-        while (true) {
-            shouldContinue = tick();
-            if (!shouldContinue) {
-                break;
+        boolean shouldContinue = true; 
+        
+        /* two options for this I see. we have a while loop for keep going until user says stop, and an inside for loop that doesnt need the tick funtion
+        or we leave it like this and just let the while loop get checked every second after a tick
+        */
+        while(shouldContinue)
+        {
+            if()//check for breakpoints here
+            {
+                //if found, stop and show what was found
+            }
+            else//if not found run the simulation like normal
+            {
+                shouldContinue = tick();//tick updates world each time it is called.
+                if(!shouldContinue)//goers in if its the end of the day
+                {
+                   //show options screen here
+                
+                    //check last if user wants to go anohter day. if not leave should continue as false, otherwise make it true
+                
+                }
+                //no else because as long as tick has everything to update in it there shoiuld be nothing else happening here
             }
         }
     }
@@ -25,7 +42,7 @@ public class TrainSimulator {
      */
     public static boolean tick() {
         // Update world (train locations, and others)
-        boolean shouldStop = updateWorld();
+        updateWorld();
         TrainSimulator.currentTime += 1;
         
         // Check if we need to pause and get user input
@@ -36,11 +53,61 @@ public class TrainSimulator {
         return true;
     }
 
-    public static boolean updateWorld() {
+    public static void updateWorld() {
         System.out.print('.');
-        return true;
+        
         // update train locations and status
         // update routing for trains
         // update passenger statuses
+        
+        //check if holiday/weekend first (probably based on days, so once we know how we can write something here)
+            //grab either normal or holiday or weekend schedule
+                int whichType = 0;
+                getSchedule(0);
+        
+        //check for weather (Either in schedule or basedon days)
+        boolean badWeather = false;//assum,ing there is onlky bad or good weather. otherwise I would recomend ints
+        //increment trains based on weather
+        if(badWeather)
+        {
+            //increment based on bad weather
+        }
+        else
+        {
+            //increment trains         
+        }
+        updatePassengers();
+        updateStats();
     }
+    
+    private static void getSchedule(int scheduleType)//if 0, its normal schedule, 1 is weekend,and 3 is holiday
+    {
+        //will have to retunr/hold the schedule in some way so its easier to iterate through. 
+    }
+    
+    public static boolean breakpointReached()
+    {
+        boolean answer = false;
+        //check if breakpoint was reached
+        //may need to have a parameter for the function that is the break[point condition its checking if true
+        return answer;
+    }
+    
+    public static void updateRoutes()//could maybe be private
+    {
+        //call the algorithm to update trains routes
+    }
+    
+    public static void updateStats()
+    {
+        //send all relevant info to the databse
+        
+    }
+    
+    public static void updatePassengers()//maybe should have a parameter of what train it is effecting
+    {
+        //how many off the train and how many added
+    }
+    
+    
 }
